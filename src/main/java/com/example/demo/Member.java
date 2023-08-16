901b53ad26f0fecf4a1890b069a6db7de963fb7a
choikangheon <kangheon3@gmail.com>
2023-08-16 18:33:36 +0900

    [add] Logging System init

---
+++ src/main/java/com/example/demo/Member.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Table(name = "MEMBER")
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "AGE")
+    private Integer age;
+
+}
