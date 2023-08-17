658f47d19ed26705e6fd3550e6b2bfcf38cc21cf
choikangheon <kangheon3@gmail.com>
2023-08-17 17:26:42 +0900

    [ADD] MDC filter 추가

---
+++ src/main/java/com/example/demo/logging/MDCLoggingFilter.java
@@ -0,0 +1,27 @@
+package com.example.demo.logging;
+
+import org.slf4j.MDC;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+class MDCLoggingFilter implements Filter {
+
+    @Override
+    public void doFilter(final ServletRequest servletRequest, final ServletResponse servletResponse, final FilterChain filterChain) throws IOException, ServletException {
+        final UUID uuid = UUID.randomUUID();
+        MDC.put("traceId", uuid.toString());
+        filterChain.doFilter(servletRequest, servletResponse);
+        MDC.clear();
+    }
+}
